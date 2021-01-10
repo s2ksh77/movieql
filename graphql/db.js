bbca9660476fc3b23ec0b666cc977e0d5618d89d
soohyun <s2ksh_77@naver.com>
2021-01-10 21:39:53 +0900

    #13 Create GraphQL Server withd REST API get Movies

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,72 @@
-export const people = [
-    {
-        id: '0',
-        name: 'soohyun',
-        age: 31,
-        gender: 'male'
-    },
-    {
-        id: '1',
-        name: 'nico',
-        age: 13,
-        gender: 'male'
-    },
-    {
-        id: '2',
-        name: 'nico22',
-        age: 14,
-        gender: 'male'
-    },
-    {
-        id: '3',
-        name: 'nico33',
-        age: 15,
-        gender: 'male'
-    },
-    {
-        id: '4',
-        name: 'nico44',
-        age: 165,
-        gender: 'male'
-    },
-    {
-        id: '5',
-        name: 'nico55',
-        age: 161,
-        gender: 'male'
-    },
-    {
-        id: '6',
-        name: 'nico66',
-        age: 17,
-        gender: 'male'
-    }
-]
+import axios from 'axios';
+const BASE_URL = "https://yts.mx/api/v2/"
+const LIST_MOVIES_URL = `${BASE_URL}list_movies/json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+export const getMovies = async(limit, rating) =>{
+    const {
+        data : {
+            data : { movies }
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        param: {
+            limit,
+            minimum_rating: rating
+        }
+    })
+    return movies;
 }
+
+export const getMovie = async id =>{
+    const {
+        data: {
+            data: { movie }
+        }
+    } = await axios(MOVIE_DETAIL_URL, {
+        params:{
+            movie_id: id
+        }
+    })
+    return movie;
+}
+
+export const getSuggestions = async id =>{
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+        params:{
+            movie_id: id
+        }
+    })
+    return movies;
+}
+
+//export const getMovies = () => movies;
+
+//export const getById = id => {
+//    const filteredMovies = movies.filter(movie => movie.id === String(id));
+//    return filteredMovies[0];
+//}
+
+//export const deleteMovie = id => {
+//    const cleanMovies = movies.filter(movie => movie.id !== String(id));
+//    if(movies.length > cleanMovies.length){
+//        movies = cleanMovies;
+//       return true;
+//    }else{
+//        return false;
+//    }
+//}
+
+//export const addMovie = (name, score) =>{
+//    const newMovie = {
+//       id: `${movies.length + 1}`,
+//        name,
+//        score
+//    };
+//    movies.push(newMovie);
+//    return newMovie;
+//}
